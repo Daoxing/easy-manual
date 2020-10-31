11bd88967f7bc4407c29d1c3ab8f26a36f120384
Daoxing <li.dao@husky.neu.edu>
2020-10-31 12:32:07 +0800

    [EM-22]update group,invite user to group,approve user to group,delete user from group

--- src/schema/group/typeDef.ts
+++ src/schema/group/typeDef.ts
@@ -4,9 +4,51 @@ type Group{
     group_nme: String
     group_intro: String
     created_user: User
+    all_joined_members: GroupMembers
+    join_group_request: joinGroupRequest 
+}
+
+type GroupMembers{
+    count:Int
+    members:[User]
+}
+
+type joinGroupRequest{
+    count:Int
+    members:[User]
 }
 
 type createGroupResult{
+    success: Boolean!
+    message: String
+    result: Group
+}
+
+type updateGroupResult{
+    success: Boolean!
+    message: String
+    result: Group
+}
+
+type inviteUserToGroupResult{
+    success: Boolean!
+    message: String
+    result: Group
+}
+
+type approveUserToGroupResult{
+    success: Boolean!
+    message: String
+    result: Group
+}
+
+type rejectUserToGroupResult{
+    success: Boolean!
+    message: String
+    result: Group
+}
+
+type deleteGroupResult{
     success: Boolean!
     message: String
     result: Group
@@ -17,12 +59,42 @@ input createGroupInput{
     group_intro:String
 }
 
+input updateGroupInput{
+    group_id:ID!
+    group_nme:String!
+    group_intro:String
+}
+
+input inviteUserToGroupInput{
+    user_id:ID!
+    group_id:ID!
+}
+
+input approveUserToGroupInput{
+    user_id:ID!
+    group_id:ID!
+}
+
+input rejectUserToGroupInput{
+    user_id:ID!
+    group_id:ID!
+}
+
 extend type Query{
-    myGroups:[Group]!
+    myGroups:[Group!]
+    searchGroupByName(name:String!):[Group!]
 } 
 
 extend type Mutation{
-    createGroup(groupInfo:createGroupInput): createGroupResult
+    createGroup(groupInfo:createGroupInput!): createGroupResult
+    updateGroup(groupInfo:updateGroupInput!): updateGroupResult
+
+    inviteUserToGroup(inviteInfo:inviteUserToGroupInput!): inviteUserToGroupResult
+
+    approveUserToGroup(approveInfo:approveUserToGroupInput!): approveUserToGroupResult
+    rejectUserToGroup(rejectInfo:approveUserToGroupInput!): rejectUserToGroupResult
+
+    deleteGroup(groupId:ID!):deleteGroupResult
 }
 
 `;
---
+++ src/schema/types.ts
@@ -0,0 +1,5 @@
+export interface graphqlResult {
+  success: boolean;
+  message: string;
+  result: any;
+}
